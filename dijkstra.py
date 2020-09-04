fb20a4f6e6e8eb7ae77071bfc65d415cfc059711
ausaki <ljm51689@gmail.com>
2020-09-04 16:51:21 +0800

    update

--- dijkstra.py
+++ dijkstra.py
@@ -1,20 +1,14 @@
-def dijkstra():
-    costs = [10000] * N
-    costs[K - 1] = 0
-    queue = [(0, K)]
-    seen = set()
-    graph = collections.defaultdict(dict)
-    for u, v, w in times:
-        graph[u][v] = w
-    while queue:
-        t1, i = heapq.heappop(queue)
-        if i in seen:
+def bfs(i, n):
+    q = [(0, i)]
+    dist = [float('inf')] * n
+    visited = {i}
+    while q:
+        w, j = heapq.heappop(q)
+        if w > dist[j]:
             continue
-        seen.add(i)
-        costs[i - 1] = t1
-        for j, t2 in graph[i].items():
-            if j in seen:
-                continue
-            heapq.heappush(queue, (t1 + t2, j))
-    m = max(costs)
-    return m if m != 10000 else -1
+        visited.add(j)
+        for k, w_ in g[j]:
+            d = w + w_
+            if k not in visited and d < dist[k]:
+                heapq.heappush(q, (d, k))
+                dist[k] = d
